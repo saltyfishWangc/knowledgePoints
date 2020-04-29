dfb7db253ee2c466c87ecd2020e861be2474b71c
saltyfishWangc <741627532@qq.com>
2020-04-29 23:47:58 +0800

    阻塞队列

---
+++ src/main/java/com/wangc/knowledgepoints/concurrent/Volatile/VolatileTest.java
@@ -0,0 +1,8 @@
+package com.wangc.knowledgepoints.concurrent.Volatile;
+
+/**
+ * https://www.cnblogs.com/dolphin0520/p/3920373.html
+ */
+public class VolatileTest {
+    // 假设对共享变量除了赋值之外并不完成其他操作， 那么可以将这些共享变量声明为 volatile
+}
---
+++ src/main/java/com/wangc/knowledgepoints/concurrent/blockingqueue/ArrayBlockingQueueTest.java
@@ -0,0 +1,4 @@
+package com.wangc.knowledgepoints.concurrent.blockingqueue;
+
+public class ArrayBlockingQueueTest {
+}
